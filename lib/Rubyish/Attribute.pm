573ab126289e73af983de6e09591f29a7387567a
shelling <navyblueshellingford@gmail.com>
2008-11-12 01:13:29 +0800

    add clear synopsis

--- lib/Rubyish/Attribute.pm
+++ lib/Rubyish/Attribute.pm
@@ -1,7 +1,5 @@
-
-
 =head1 NAME
-    autobox::Accessor - this provide ruby-like accessor builder: attr_accessor, attr_writer and attr_reader.
+    autobox::Accessor - provide ruby-like accessor builder: attr_accessor, attr_writer and attr_reader.
 =cut
 package Rubyish::Attribute;
 
@@ -18,17 +16,44 @@ Sub::Exporter::setup_exporter({
 our $VERSION = "0.01";
 
 =head1 SYNOPSIS
-    package Animal;
-    
-    use Rubyish::Attribute qw(:all);
+    #!/usr/bin/env perl
+   
+    use 5.010;
 
-    attr_accessor( [qw(name address jobs resume)] )
+    use strict;
+    use warnings;
 
-    1;
+    {
+        package Animal;
+        
+        use Rubyish::Attribute qw(:all); # use :all to import attr_accessor, attr_writer and attr_reader
+
+        attr_accessor( [qw(name color type)] ); # pass a arrayref as the only one parameter
+        # now we have the getter double as setter
+        # named as name, color and type
+
+        # then create a constructer based on hashref
+        sub new {
+            $class = shift;
+            bless {}, $class;
+        }
+
+        1;
+    }
+   
+    # because all setter return instance itself
+    # now we can manipulate object in ruby way more than ruby
+    $dogy = Animal->new()->name("rock")->color("black")->type("unknown");
+
+    # use reader to access instance data
+    say $dogy->name();  # rock
+    say $dogy->color(); # black
+    say $dogy->type();  # unknown
 =cut
 
 =head1 FUNCTION
-
+    
+=cut
 
 =head2 attr_accessor
 
@@ -57,7 +82,11 @@ sub attr_accessor {
 }
 
 =head2 attr_reader
-
+    attr_reader create only getter for the class you call it.
+    for example, if we call attr_reader([qw(name)]) in Animal package,
+    and $dogy = Animal->new({name => "rock"}).
+    Now $dogy->name() return the dog's name "rock",
+    but $dogy->name("jack") would cause an exception.
 =cut
 sub attr_reader {
     no strict;
@@ -106,9 +135,10 @@ sub attr_writer {
 
 =head1 AUTHOR
     shelling <navyblueshellingford at gmail.com>
+    gugod    <gugod at gugod.org>
 =cut
 
 =head1 BUGS
-    please report bugs to shelling at cpan.org
+    please report bugs to <shelling at cpan.org> or <gugod at gugod.org>
 =cut
 1;
